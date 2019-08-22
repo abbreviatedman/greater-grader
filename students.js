cc53a46e2bfc7d8d44f292b0b8c4e1c11134bdda
Colin Jaffe <balloonasaurus@gmail.com>
2019-08-22 14:19:54 -0400

    Takes away test students and adds directions.

--- students.js
+++ students.js
@@ -1,20 +1,18 @@
 const students = [
-  {
-    name: 'Colin',
-    term: 4,
-    grades: [100, 90, 80, 95],
-  },
-  {
-    name: 'Mesuara',
-    term: 5,
-    grades: [100, 70, 95, 95],
-  },
 ];
 
 function newStudent() {
   return {
-    name: '',
+    // TODO: make it so that all new students are not named Bill.
+    name: 'Bill',
     term: 1,
     grades: [],
-}
+
+    // TODO: `getAverage` method that returns the average of all the grades.
+
+
+    // TODO: `addGrade` method that adds a given grade to the grades list.
+
+
+  }
 }
